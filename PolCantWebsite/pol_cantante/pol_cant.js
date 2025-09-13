92f2acf71169d66274849654a8610dbdcf2c52a8
Novanoid𓆱 <bernardeugeniuszkk@gmail.com>
2025-09-13 12:20:13 +0000

     

--- PolCantWebsite/pol_cantante/pol_cant.js
+++ PolCantWebsite/pol_cantante/pol_cant.js
@@ -20,8 +20,8 @@ function applyData(file) {
     for (let key in file) {
         let el = document.getElementById(key)
         if (el) {
-            if (/prz[0-3]b/.test(file[key])) el.innerHTML = "€" + file[key]
-            else el.innerHTML = file[key]
+            if (/prz[0-3]b/.test(key)) el.innerHTML = file[key].replace(": e", ": €");
+            else el.innerHTML = file[key];
         }
     }
 }
