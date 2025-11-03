f4d7c26b0c6b4d7a19cdbeb85ab26a344fc5c562
Novanoid𓆱 <bernardeugeniuszkk@gmail.com>
2025-11-03 06:26:35 +0000

    polcant resume

--- PolCantWebsite/pol_cantante/pol_cant.js
+++ PolCantWebsite/pol_cantante/pol_cant.js
@@ -1,5 +1,5 @@
-const chosen_lang = "Pl";
-const default_lang = "Pl";
+let current_lang;
+let langs = ["PL", "EN", "NL", "FR"];
 
 async function getData() {
     let data = await fetch(`https://api.github.com/repos/Novanoid2/Random/contents/PolCantWebsite/pol_cantante/info.json`)
@@ -20,58 +20,51 @@ async function applyData(file) {
     for (let key in file) {
         let el = document.getElementById(key)
         if (el) {
-            if (/prz[0-3]b/.test(key)) el.innerHTML = (file[key].replace(" e", " €"));
+            if (/prz[0-3]b/.test(key)) el.innerHTML = file[key].replace(" e", " €");
             else el.innerHTML = file[key];
         }
     }
 }
 
+if (document.getElementById("zdjecia").textContent.trim().toLowerCase() === "zdjęcia") { current_lang = "PL"; langs.splice(0, 1); }
+else if (document.getElementById("zdjecia").textContent.trim().toLowerCase() === "pictures") { current_lang = "EN"; langs.splice(1, 1); }
+else if (document.getElementById("zdjecia").textContent.trim().toLowerCase() === "fotos") { current_lang = "NL"; langs.splice(2, 1); }
+else if (document.getElementById("zdjecia").textContent.trim().toLowerCase() === "photos") { current_lang = "FR"; langs.splice(3, 1); }
+
 document.getElementById("lang").onclick = function () {
     if (!document.body.contains((document.getElementById("langBox")))) {
         let lang = document.getElementById("lang").getBoundingClientRect();
         let div = document.createElement("div");
-        if (window.innerWidth - (lang.left + lang.width / 2 - 50) < 100) div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 90}px; background-color: white; height: 170px; width: 100px; z-index: 2; border-radius: 20px; opacity: 0; transition: opacity 0.7s;`;
-        else div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 50}px; background-color: white; height: 170px; width: 100px; z-index: 2; border-radius: 20px; opacity: 0; transition: opacity 0.7s;`;
+        if (window.innerWidth - (lang.left + lang.width / 2 - 50) < 100) div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 90}px; background-color: white; height: 170px; width: 98px; z-index: 2; border-radius: 20px; opacity: 0; transition: opacity 0.7s;`;
+        else div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 50}px; background-color: white; height: 170px; width: 98px; z-index: 2; border-radius: 20px; opacity: 0; transition: opacity 0.7s;`;
         div.id = "langBox";
         div.innerHTML = `<ul>
-          <a href="" style="margin-left: -24px; font-family: Kepler; font-size: 1.8rem;"><strong>>PL<</strong></a>
+          <a href="http://127.0.0.1:5500/pol_cantante/pol_cant${current_lang}.html" style="margin-left: -24px; font-family: Kepler; font-size: 1.8rem;"><strong>>${current_lang}<</strong></a>
           <br>
-          <a href="" style="margin-left: -11px; font-family: Kepler; font-size: 1.8rem;">EN</a>
+          <a href="http://127.0.0.1:5500/pol_cantante/pol_cant${langs[0]}.html" style="margin-left: -11px; font-family: Kepler; font-size: 1.8rem;">${langs[0]}</a>
           <br>
-          <a href="" style="margin-left: -10px; font-family: Kepler; font-size: 1.8rem;">NL</a>
+          <a href="http://127.0.0.1:5500/pol_cantante/pol_cant${langs[1]}.html" style="margin-left: -10px; font-family: Kepler; font-size: 1.8rem;">${langs[1]}</a>
           <br>
-          <a href="" style="margin-left: -11px; font-family: Kepler; font-size: 1.8rem;">FR</a>
+          <a href="http://127.0.0.1:5500/pol_cantante/pol_cant${langs[2]}.html" style="margin-left: -11px; font-family: Kepler; font-size: 1.8rem;">${langs[2]}</a>
         </ul>`;
         document.body.appendChild(div);
-        setTimeout(() => { div.style.opacity = 1; }, 10);
+        setTimeout(() => div.style.opacity = 1, 10);
     } else document.body.removeChild(document.getElementById("langBox"));
 }
 
-document.getElementById('onas').onclick = () => {
-    document.getElementById('boxONas-border').scrollIntoView({ behavior: 'smooth' });
-};
+document.getElementById('onas').onclick = () => document.getElementById('boxONas-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
-document.getElementById('koncert').onclick = () => {
-    document.getElementById('boxKoncerty-border').scrollIntoView({ behavior: 'smooth' });
-};
+document.getElementById('koncert').onclick = () => document.getElementById('boxKoncerty-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
-/*document.getElementById('bilet').onclick = () => {
-    document.getElementById('boxBilety-border').scrollIntoView({ behavior: 'smooth' });
-};*/
+//document.getElementById('bilet').onclick = () => document.getElementById('boxBilety-border').scrollIntoView({ behavior: 'smooth' });
 
-document.getElementById('pomoc').onclick = () => {
-    document.getElementById('boxPomoc-border').scrollIntoView({ behavior: 'smooth' });
-};
+document.getElementById('pomoc').onclick = () => document.getElementById('boxPomoc-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
-document.getElementById('zdjecia').onclick = () => {
-    document.getElementById('boxZdjecia-border').scrollIntoView({ behavior: 'smooth' });
-};
+document.getElementById('zdjecia').onclick = () => document.getElementById('boxZdjecia-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
-document.getElementById('dolacz').onclick = () => {
-    document.getElementById('boxDolacz-border').scrollIntoView({ behavior: 'smooth' });
-};
+document.getElementById('dolacz').onclick = () => document.getElementById('boxDolacz-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
 //window.addEventListener('resize', () => { location.reload(); });
 
-document.documentElement.style.setProperty(`--dl`, `${default_lang}`);
+document.documentElement.style.setProperty(`--dl`, `Pl`);
 console.log("%c Hello! watch'ya doing here? ", 'background: #222; color: #bada55; font-size: 22px;');
