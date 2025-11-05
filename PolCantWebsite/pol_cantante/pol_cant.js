9fd59347d603806a76b18b23c6616580ee06980c
Novanoid𓆱 <bernardeugeniuszkk@gmail.com>
2025-11-05 20:24:24 +0000

    v0.5

--- PolCantWebsite/pol_cantante/pol_cant.js
+++ PolCantWebsite/pol_cantante/pol_cant.js
@@ -1,5 +1,6 @@
-let current_lang;
-let langs = ["PL", "EN", "NL", "FR"];
+let current_lang = navigator.language.toUpperCase();
+let langs = [`PL`, "EN", "NL", "FR"];
+let boxesExist = false;
 
 async function getData() {
     let data = await fetch(`https://api.github.com/repos/Novanoid2/Random/contents/PolCantWebsite/pol_cantante/info.json`)
@@ -22,12 +23,11 @@ async function applyData(file) {
         if (el) {
             if (/prz[0-3]b/.test(key)) el.innerHTML = file[key].replace(" e", " €");
             else el.innerHTML = file[key];
-
         }
     }
 }
 
-for (let lang in arr = ["zdjęcia", "pictures", "fotos", "photos"]) {
+for (let lang in arr = ["zdjęcia", "pictures", "fotos", "photos"]) {//PL, EN, NL, FR
     if (document.getElementById("zdjecia").textContent.trim().toLowerCase() === arr[lang]) {
         current_lang = langs[lang];
         langs.splice(lang, 1);
@@ -36,10 +36,20 @@ for (let lang in arr = ["zdjęcia", "pictures", "fotos", "photos"]) {
 
 document.getElementById("lang").onclick = () => {
     if (!document.body.contains((document.getElementById("langBox")))) {
+        if ((document.body.contains(document.getElementById("contactInfoBox")))) document.body.removeChild(document.getElementById("contactInfoBox"));
+        if ((document.body.contains(document.getElementById("pomocBox")))) document.body.removeChild(document.getElementById("pomocBox"));
+
         const lang = document.getElementById("lang").getBoundingClientRect();
         const div = document.createElement("div");
-        if (window.innerWidth - (lang.left + lang.width / 2 - 50) < 100) div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 90}px; background-color: white; height: 170px; width: 98px; z-index: 2; border-radius: 20px; opacity: 0; transition: opacity 0.7s;`;
-        else div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 50}px; background-color: white; height: 170px; width: 100px; z-index: 1; border-radius: 20px; opacity: 0; transition: opacity 0.7s;`;
+
+        if (window.innerWidth - (lang.left + lang.width / 2 - 50) < 100) div.style = `position: absolute;
+        top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 90}px; background-color: white;
+        height: 170px; width: 98px; z-index: 2; border-radius: 20px; opacity: 0; transition: opacity 0.7s; box-shadow: 0px 0px 25px black`;
+
+        else div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 50}px;
+        background-color: white; height: 170px; width: 100px; z-index: 1; border-radius: 20px; opacity: 0;
+        transition: opacity 0.7s; box-shadow: 0px 0px 25px black`;
+
         div.id = "langBox";
         div.innerHTML = `<ul>
           <a href="http://127.0.0.1:5500/PolCantWebsite/pol_cantante/pol_cant${current_lang}.html" style="margin-left: -24px; font-family: Kepler; font-size: 1.8rem;"><strong>>${current_lang}<</strong></a>
@@ -50,43 +60,38 @@ document.getElementById("lang").onclick = () => {
           <br>
           <a href="http://127.0.0.1:5500/PolCantWebsite/pol_cantante/pol_cant${langs[2]}.html" style="margin-left: -11px; font-family: Kepler; font-size: 1.8rem;">${langs[2]}</a>
         </ul>`;
+
         document.body.appendChild(div);
         setTimeout(() => div.style.opacity = 1, 10);
     } else document.body.removeChild(document.getElementById("langBox"));
 }
 
-document.getElementById("DolaczBut").onclick = () => {
-    if (!document.getElementById("boxDolacz-border").contains((document.getElementById("contactInfoBox")))) {
-        const buttonPos = document.getElementById("DolaczBut").getBoundingClientRect();
-
-        document.getElementById("DolaczBut").style = `transform: translate(-108px,65px);`;
-        document.getElementById("DolaczBut").textContent = "Dołącz później";
-        document.getElementById("boxDolacz-border").style.height = "350px";
-
+document.getElementById("dolacz").onclick = () => {
+    if (!document.body.contains((document.getElementById("contactInfoBox")))) {
+        if ((document.body.contains(document.getElementById("langBox")))) document.body.removeChild(document.getElementById("langBox"));
+        if ((document.body.contains(document.getElementById("pomocBox")))) document.body.removeChild(document.getElementById("pomocBox"));
+        const buttonPos = document.getElementById("dolacz").getBoundingClientRect();
         const div = document.createElement("div");
-        div.style = `position: relative; transform: translateY(40%); left: ${buttonPos.left + buttonPos.width / 2 - 250}px;
-        height: 200px; width: 500px; z-index: 1; opacity: 0; transition: opacity 0.7s;
-        font-size: 1.8rem; font-weight: bold; text-align: center; display: flex; opacity: 0; transition: opacity 1s;`;
+
+        //holy shit why is this so long
+        div.style = `position: absolute; left: ${buttonPos.left + buttonPos.width / 2 - 175}px;
+        top: ${buttonPos.top + buttonPos.height + 20}px; background-color: white; height: 175px; width: 355px;
+        z-index: 1; opacity: 0; transition: opacity 0.7s; border-radius: 20px; font-size: 1.5rem;
+        font-weight: bold; text-align: center; opacity: 0; transition: opacity 1s; box-shadow: 0px 0px 25px black`;
+
         div.id = "contactInfoBox";
-        div.innerHTML = `<hr>
-        Kontakt z dyrygentką:
+        div.innerHTML = `<hr style="border: none;">
+              Kontakt z nami (dyrygentka): 
+            <hr style="width: 80%;">
+            &middot; +32 nie wiem
             <br>
+              lub
             <br>
-            Numer telefonowy: +32 nie wiem
-            <br>
-            lub
-            <br>
-            E-mail: jakis_tam@email.com
-            <br>
-        <hr>`;
-        document.getElementById("boxDolacz-border").appendChild(div);
+            &middot; jakis_tam@email.com`;
+
+        document.body.appendChild(div);
         setTimeout(() => div.style.opacity = 1, 10);
-    } else {
-        document.getElementById("boxDolacz-border").removeChild(document.getElementById("contactInfoBox"));
-        document.getElementById("DolaczBut").style = `transform: translate(-101px,80px);`;
-        document.getElementById("boxDolacz-border").style.height = "200px";
-                document.getElementById("DolaczBut").textContent = "Dołącz teraz";
-    }
+    } else document.body.removeChild(document.getElementById("contactInfoBox"));
 }
 
 document.getElementById('onas').onclick = () => document.getElementById('boxONas-border').scrollIntoView({ behavior: "smooth", block: "center" });
@@ -95,13 +100,13 @@ document.getElementById('koncert').onclick = () => document.getElementById('boxK
 
 //document.getElementById('bilet').onclick = () => document.getElementById('boxBilety-border').scrollIntoView({ behavior: 'smooth' });
 
-document.getElementById('pomoc').onclick = () => document.getElementById('boxPomoc-border').scrollIntoView({ behavior: "smooth", block: "center" });
+//document.getElementById('pomoc').onclick = () => document.getElementById('boxPomoc-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
 document.getElementById('zdjecia').onclick = () => document.getElementById('boxZdjecia-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
-document.getElementById('dolacz').onclick = () => document.getElementById('boxDolacz-border').scrollIntoView({ behavior: "smooth", block: "center" });
+//document.getElementById('dolacz').onclick = () => document.getElementById('boxDolacz-border').scrollIntoView({ behavior: "smooth", block: "center" });
 
-//window.addEventListener('resize', () => { location.reload(); });
+window.addEventListener('resize', () => { location.reload(); });
 
 document.documentElement.style.setProperty(`--dl`, `Pl`);
 console.log("%c Hello! watch'ya doing here? ", 'background: #222; color: #bada55; font-size: 22px;');
