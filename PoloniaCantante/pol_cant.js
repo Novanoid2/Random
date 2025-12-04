e9bfdd40a2486a8a73fad76686cd8db35fa6d551
Novanoid𓆱 <bernardeugeniuszkk@gmail.com>
2025-12-04 14:48:25 +0000

    0.6.2 1.4.2 (name change)

--- PoloniaCantante/pol_cant.js
+++ PoloniaCantante/pol_cant.js
@@ -1,13 +1,16 @@
-/*current todo: finish proby, ask if anything more to add / for custom fonts and color and stuff, popraw css zeby automatycznie bylo pod soba,
-make seperate web for bilety, search for bugs/things to shorten, translate to other langs and done*/
+/*current todo: finish proby(make a second swiper somehow), find out why changing width theres a bar on the bottom? (something to do w .boxes), 
+make seperate web for bilety, search for bugs/things to shorten translate to other langs and done*/
 //change the popping-up info boxes to html w "aria-hidden" or whatev and not a long ass javascript command lol
+//for later -> use document.getElementById("test").removeAttribute("hidden"); because i just tested it and it works! yippie
 
 function pushErrorDiv() {
     const errorDiv = document.createElement("div");
     errorDiv.textContent = "We have encountered an error, some parts of the website may not work correctly, we are working on a fix.";
-    errorDiv.style = "position: absolute; top: 200px; left: 0; width: 100%; text-align: center; background-color: red; color: white; font-size: 1.4rem; padding: 10px; z-index: 3;";
+    errorDiv.style = "position: absolute; top: 200px; width: 80%; text-align: center; background-color: red; color: white; font-size: 1.3vw; padding: 10px; z-index: 1; left: 50%; transform: translateX(-50%);";
     document.body.appendChild(errorDiv);
 }
+
+getData();
 
 async function getData() {
     try {
@@ -23,8 +26,6 @@ async function getData() {
         pushErrorDiv();
     }
 }
-
-getData();
 
 async function applyData(file) {
     for (let key in file) {
@@ -48,11 +49,11 @@ document.getElementById("lang").onclick = () => {
         const div = document.createElement("div");
         //holy shit why is this so long
         if (window.innerWidth - (lang.left + lang.width / 2 - 50) < 100) div.style = `position: absolute;
-        top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 90}px; background-color: white;
+        top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 87}px; background-color: white;
         height: auto; width: 90px; border-radius: 20px; opacity: 0; transition: opacity 0.8s;
         box-shadow: 0px 0px 25px black font-family: Palatino`;
 
-        else div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 50}px;
+        else div.style = `position: absolute; top: ${lang.top + lang.height + 10}px; left: ${lang.left + lang.width / 2 - 47}px;
         background-color: white; height: auto; width: 90px; border-radius: 20px; opacity: 0;
         transition: opacity 0.8s; box-shadow: 0px 0px 25px black; font-family: Palatino; z-index: 2;`;
 
@@ -134,8 +135,6 @@ document.getElementById('proby').onclick = () => document.getElementById('boxPro
 
 document.getElementById('zdjecia').onclick = () => document.getElementById('boxZdjecia').scrollIntoView({ behavior: "smooth", block: "center" });
 
-//document.getElementById('dolacz').onclick = () => document.getElementById('boxDolacz-border').scrollIntoView({ behavior: "smooth", block: "center" });
-
 for (let lang in arr = ["zdjęcia", "pictures", "fotos", "photos"]) {//PL, EN, NL, FR
     if (document.getElementById("zdjecia").textContent.trim().toLowerCase() === arr[lang]) {
         current_lang = langs[lang];
@@ -143,7 +142,11 @@ for (let lang in arr = ["zdjęcia", "pictures", "fotos", "photos"]) {//PL, EN, N
     }
 }
 
+if (window.innerWidth < 545) document.getElementById("h1").innerHTML = `Witamy na stronie<br>Polonia Cantante!`
+
+//if (window.innerWidth < 1050)
+
 window.addEventListener('resize', () => { location.reload(); });
 
-document.documentElement.style.setProperty(`--dl`, `Pl`);
+document.documentElement.style.setProperty(`--dl`, `PL`);
 console.log("%c Hello! watch'ya doing here? ", 'background: #222; color: #bada55; font-size: 22px;');
